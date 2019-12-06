7bea33b74ce1d5f969f8de9b3339d73f686fd2cd
CraigWhittington <cfwhit@hotmail.com>
2019-12-06 15:47:47 +0000

    Basics

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,60 @@
 import React from "react";
-import { Link } from "gatsby";
+import { Link, graphql } from "gatsby";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import { FaGulp } from "react-icons/fa";
+import Info from "../components/Home/Info";
+import Products from "../components/Home/Products";
+import Contact from "../components/Home/Contact";
+import Menu from "../components/Home/Menu";
 
-const IndexPage = () => (
+import BackgroundSection from "../components/Globals/BackgroundSection";
+
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <FaGulp />
+    <BackgroundSection 
+      img={data.img.childImageSharp.fluid}
+      title="home-roast"
+    />
+    <Info />
+    <Menu items={data.menu}/>
+    <Products />
+    <Contact/>
   </Layout>
 );
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "default-background.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    menu: allContentfulCoffeeItem {
+      edges {
+        node {
+          id
+          title
+          description {
+            description
+          }
+          price
+          category
+          image {
+            fixed(width: 50, height: 50) {
+              ...GatsbyContentfulFixed_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+
 
 export default IndexPage;
